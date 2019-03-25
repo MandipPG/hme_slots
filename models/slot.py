57c497916fb95306069c6098e2b2cd267c8455d9
Suyash Soni <suyash.soni248@gmail.com>
2019-03-25 13:18:12 +0530

    Refactoring

--- models/slot.py
+++ models/slot.py
@@ -7,7 +7,9 @@ import json
 from commons.exceptions import InvalidSlotError
 
 class Slot(object):
-
+    """
+    Represents a slot.
+    """
     def __init__(self, start_time: struct_time, end_time: struct_time):
         self.__start_time__ = start_time
         self.__end_time__ = end_time
@@ -19,6 +21,13 @@ class Slot(object):
         self.end: datetime = None
 
     def make(self, current_time: datetime):
+        """
+        Attach the date information to time information in order to mark the slot as `Assigned`. e.g.
+        
+        self.start = start_date + self.__start_time
+        self.end = end_date + self.__end_time
+        
+        """
         self.start = datetime(year=current_time.year,month=current_time.month, day=current_time.day,
                                 hour=self.__start_time__.tm_hour, minute=self.__start_time__.tm_min,
                                 second=self.__start_time__.tm_sec)
@@ -28,10 +37,16 @@ class Slot(object):
 
     @property
     def is_valid(self):
+        """
+        Checks if slot is valid or not. If slot duration is more than 24 hours then slot is considered as invalid.
+        """
         return self.__end_time__ > self.__start_time__
 
     @property
     def to_dict(self):
+        """
+        Converts the underlying slot to dictionary so that it can be serialized/deserialized later, if required.
+        """
         return {
             'start_time': self.start.strftime('%Y-%m-%d %H:%M:%S'),
             'end_time': self.end.strftime('%Y-%m-%d %H:%M:%S')
